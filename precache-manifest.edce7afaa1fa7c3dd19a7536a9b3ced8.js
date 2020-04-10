7d9c0d8ed17b98f3e583f8455317bfde7be4f288
DVassilev <DVassilev@users.noreply.github.com>
2020-04-11 00:13:47 +1000

    Uploaded react-player demo

---
+++ precache-manifest.edce7afaa1fa7c3dd19a7536a9b3ced8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce3b81ff04d46c4d65863e0ecc88e74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "433953f4b56d97ffa44b",
+    "url": "/static/css/main.350ba542.chunk.css"
+  },
+  {
+    "revision": "84c4ec802592cf8ee588",
+    "url": "/static/js/2.d0853eef.chunk.js"
+  },
+  {
+    "revision": "8b5f34ee704968dec0c517b3d5f76a4f",
+    "url": "/static/js/2.d0853eef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "433953f4b56d97ffa44b",
+    "url": "/static/js/main.9f755230.chunk.js"
+  },
+  {
+    "revision": "a896469b35ab17db43f7",
+    "url": "/static/js/runtime-main.126eb33a.js"
+  }
+]);
